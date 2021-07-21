d3b5d55ff4d575bcf6e1851a884036d7dc8adfdc
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 16:03:36 +0300

    change virtual Update method to abstract in BaseObject and children

--- AsteroidsWinForms/AsteroidsWinForms/Asteroid.cs
+++ AsteroidsWinForms/AsteroidsWinForms/Asteroid.cs
@@ -35,5 +35,15 @@ namespace AsteroidsWinForms
                     break;
             }
         }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
     }
 }
--- AsteroidsWinForms/AsteroidsWinForms/Star.cs
+++ AsteroidsWinForms/AsteroidsWinForms/Star.cs
@@ -8,13 +8,23 @@ using System.Threading.Tasks;
 
 namespace AsteroidsWinForms
 {
-    class Star : Asteroid
+    class Star : BaseObject
     {
         public Star(Point pos, Point dir, Size size) : base(pos, dir, size) { }
 
         public override void Draw()
         {
             Game.Buffer.Graphics.DrawImage(new Bitmap(Resources.star3, new Size(Size.Width, Size.Height)), Pos.X, Pos.Y);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
         }
     }
 }
