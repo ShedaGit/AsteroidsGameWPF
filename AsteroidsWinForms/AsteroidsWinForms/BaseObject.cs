baae8b800ecfef45827d06d4d7d5492e1b0a437c
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 14:55:20 +0300

    add abstract class BaseObject for ingame objects + interface for collisions

---
+++ AsteroidsWinForms/AsteroidsWinForms/BaseObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AsteroidsWinForms
+{
+    abstract class BaseObject : ICollision
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+
+        public BaseObject(Point pos, Point dir, Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+
+        public Rectangle Rect => new Rectangle(Pos, Size);
+
+        public bool Collision(ICollision obj)
+        {
+            return Rect.IntersectsWith(obj.Rect);
+        }
+
+        public abstract void Draw();
+
+        public virtual void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
+    }
+}
