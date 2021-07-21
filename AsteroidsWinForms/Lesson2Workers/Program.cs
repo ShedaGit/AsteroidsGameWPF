abde7505701ee25ee9d52ba69d0e21649df4da5b
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 12:24:53 +0300

    add ShowCase in main program

---
+++ AsteroidsWinForms/Lesson2Workers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson2Workers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowCase();
+        }
+
+        static public void ShowCase()
+        {
+            var workers = new BaseWorker[]
+            {
+                new Worker("Eddie", 190),
+                new Worker("Adam", 175),
+                new Worker("John", 230),
+                new Worker("Anna", 205),
+                new OutsourceWorker("Mike", 40000)
+            };
+
+            foreach (var worker in workers)
+            {
+                Console.WriteLine($"{worker.Name}'s salary per month = {worker.GetSalaryPerMonth()}");
+            }
+
+            Console.WriteLine();
+
+            Array.Sort(workers);
+
+            foreach (var worker in workers)
+            {
+                Console.WriteLine($"{worker.Name}'s salary per month = {worker.GetSalaryPerMonth()}");
+            }
+        }
+    }
+}
