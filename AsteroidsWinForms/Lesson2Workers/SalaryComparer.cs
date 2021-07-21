d36c4e516c7f38a0a22f1a483d012f5154a5fedb
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 18:15:51 +0300

    add SalaryComparer class

---
+++ AsteroidsWinForms/Lesson2Workers/SalaryComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2Workers
+{
+    /// <summary>
+    /// Класс для сравнения заработных плат работников
+    /// </summary>
+    public class SalaryComparer : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return x.СalculateSalary() > y.СalculateSalary() ? 1 : -1;
+        }
+    }
+}
