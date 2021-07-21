90ec4826c97d115c7ef2eaa4071a7d20d8cd2259
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 12:04:16 +0300

    add abstract BaseWorker class

---
+++ AsteroidsWinForms/Lesson2Workers/BaseWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2Workers
+{
+    abstract class BaseWorker
+    {
+        protected string _name;
+        protected int _salary;
+
+        abstract public string Name { get; }
+
+        public BaseWorker(string name, int salary)
+        {
+            _name = name;
+            _salary = salary;
+        }
+
+        abstract public double GetSalaryPerMonth();
+    }
+}
