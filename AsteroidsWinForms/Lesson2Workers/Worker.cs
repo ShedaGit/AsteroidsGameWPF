2619b7916da174c9053e8d0c6c269f6ab53e64c2
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 12:04:57 +0300

    add Worker class with salary per hour

---
+++ AsteroidsWinForms/Lesson2Workers/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2Workers
+{
+    class Worker : BaseWorker
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Name of a Worker</param>
+        /// <param name="salary">Salary per hour</param>
+        public Worker(string name, int salary) : base(name, salary) { }
+
+        public override string Name { get => _name; }
+
+        public override double GetSalaryPerMonth()
+        {
+            return 20.8 * 8 * _salary;
+        }
+    }
+}
