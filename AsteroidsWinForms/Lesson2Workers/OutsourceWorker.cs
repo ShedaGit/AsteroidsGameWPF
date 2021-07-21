5a60d313a2419190118850b46185b58665f7696d
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-21 12:05:19 +0300

    add OutsourceWorker with fixed rate salary

---
+++ AsteroidsWinForms/Lesson2Workers/OutsourceWorker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2Workers
+{
+    class OutsourceWorker : BaseWorker
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Name of a Worker</param>
+        /// <param name="salary">Fixed rate salary</param>
+        public OutsourceWorker(string name, int salary) : base(name, salary) { }
+
+        public override string Name { get => _name; }
+
+        public override double GetSalaryPerMonth()
+        {
+            return _salary;
+        }
+    }
+}
