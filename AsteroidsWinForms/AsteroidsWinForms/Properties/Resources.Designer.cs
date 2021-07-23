6d7e253fc21c7104a394927e5d305245ca677d0b
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-24 00:07:58 +0300

    add Ship class realization of a spaceship in-game

--- AsteroidsWinForms/AsteroidsWinForms/Properties/Resources.Designer.cs
+++ AsteroidsWinForms/AsteroidsWinForms/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace AsteroidsWinForms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ship {
+            get {
+                object obj = ResourceManager.GetObject("ship", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap star1 {
             get {
                 object obj = ResourceManager.GetObject("star1", resourceCulture);
---
+++ AsteroidsWinForms/AsteroidsWinForms/Ship.cs
@@ -0,0 +1,61 @@
+﻿using AsteroidsWinForms.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AsteroidsWinForms
+{
+    class Ship : BaseObject
+    {
+        private readonly Bitmap _ship;
+        private int _energy = 100;
+        public event EventHandler<ShipDieEventArgs> Die;
+
+        public int Energy { get => _energy; }
+
+        public Ship(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            _ship = Resources.ship;
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(_ship, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+        }
+
+        public void DecreaseEnergy(int damage)
+        {
+            _energy -= damage;
+            //Решил немного переработать логику вызова события смерти корабля
+            if (_energy < 0)
+            {
+                ShipDeath(damage);
+            }
+        }
+
+        //Вынес вызов события смерти в отдельный метод, что отделить смерть и уменьшение энергии
+        private void ShipDeath(int damage)
+        {
+            if (Die != null)
+            {
+                Die.Invoke(this, new ShipDieEventArgs(damage));
+            }
+        }
+
+        public void MoveUp()
+        {
+            if (Pos.Y > Dir.Y) Pos.Y -= Dir.Y;
+        }
+
+        public void MoveDown()
+        {
+            //Поправка на Dir.Y чтобы не выйти за границы рабочей области
+            if (Pos.Y < Game.Height - Dir.Y) Pos.Y += Dir.Y;
+        }
+    }
+}
