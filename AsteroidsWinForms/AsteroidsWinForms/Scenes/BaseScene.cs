8d1669d23dbb2ae9a2a43ec33fe6f8cde8f86b82
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-24 11:13:02 +0300

    add in-game Scenes

---
+++ AsteroidsWinForms/AsteroidsWinForms/Scenes/BaseScene.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AsteroidsWinForms.Scenes
+{
+    public abstract class BaseScene : IScene, IDisposable
+    {
+        protected BufferedGraphicsContext _context;
+        protected Form _form;
+        public static BufferedGraphics Buffer;
+
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        public virtual void Init(Form form)
+        {
+            _form = form;
+            Graphics g;
+            _context = BufferedGraphicsManager.Current;
+            g = _form.CreateGraphics();
+            Width = _form.ClientSize.Width;
+            Height = _form.ClientSize.Height;
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            _form.KeyDown += SceneKeyDown;
+        }
+
+        public virtual void SceneKeyDown(object sender, KeyEventArgs e) { }
+
+        public virtual void Draw() { }
+
+        public virtual void Dispose()
+        {
+            Buffer = null;
+            _context = null;
+            _form.KeyDown -= SceneKeyDown;
+        }
+    }
+}
---
+++ AsteroidsWinForms/AsteroidsWinForms/Scenes/MenuScene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AsteroidsWinForms.Scenes
+{
+    public class MenuScene : BaseScene
+    {
+        public override void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawString("Меню игры", new Font(FontFamily.GenericSansSerif, 60, FontStyle.Underline), Brushes.White, 160, 100);
+            Buffer.Graphics.DrawString("<Enter> - игра", new Font(FontFamily.GenericSansSerif, 40, FontStyle.Underline), Brushes.White, 200, 200);
+            Buffer.Graphics.DrawString("<Esc> - выход", new Font(FontFamily.GenericSansSerif, 40, FontStyle.Underline), Brushes.White, 200, 300);
+            Buffer.Render();
+        }
+
+        public override void SceneKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                _form.Close();
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                SceneManager
+                    .Get()
+                    .Init<Game>(_form)
+                    .Draw();
+            }
+        }
+    }
+}
