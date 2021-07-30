cad82dfe53ce427f43f3a1e81a3c7b6686d4de2c
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-07-30 15:39:22 +0300

    add Employee class realization

--- WPF/Database/Employee.cs
+++ WPF/Database/Employee.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace Database
 {
+    /// <summary>
+    /// Сотрудник
+    /// </summary>
     class Employee
     {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// Полное имя
+        /// </summary>
+        public string FullName { get => $"{FirstName} {MiddleName} {LastName}"; }
+
+        /// <summary>
+        /// Направление департамента, в котором состоит сотрудник
+        /// </summary>
+        public Department OfficeCategory { get; set; }
+
+        public Employee() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstname">Имя</param>
+        /// <param name="lastname">Фамилия</param>
+        /// <param name="middlename">Отчество</param>
+        /// <param name="officeCategory">Направление департамента</param>
+        public Employee(string firstname, string lastname, string middlename, Department officeCategory)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            MiddleName = middlename;
+
+            OfficeCategory = officeCategory;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {MiddleName} {LastName.First()}.";
+        }
+
     }
 }
