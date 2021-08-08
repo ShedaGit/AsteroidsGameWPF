b9e431b7ff7b263b5dadb5c588d359959eff05ee
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-08-09 02:51:48 +0300

    fix auto realization for Employee class in Communication project

---
+++ WPF/WPF.Communication/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Communication.WpfService
+{
+    public partial class Employee : ICloneable
+    {
+        //Если необходимо можно реализовать различные конструкторы
+        public Employee() { }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
