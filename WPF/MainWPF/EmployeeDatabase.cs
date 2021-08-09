6e6b4d8b6518bb459014e88d1618fd01be2c7d09
Daniel Shelestov <fellowers.flobot@gmail.com>
2021-08-09 03:08:23 +0300

    update EmployeeDatabase logic

--- WPF/MainWPF/EmployeeDatabase.cs
+++ WPF/MainWPF/EmployeeDatabase.cs
@@ -11,92 +11,47 @@ namespace MainWPF
 {
     class EmployeeDatabase
     {
-        private const string ConnectionString = @"Data Source=(local);Initial Catalog=WPF;User ID=WPF_Root;Password=321123";
+        WpfServiceSoapClient wpfServiceSoapClient = new WpfServiceSoapClient();
 
         public ObservableCollection<Employee> Employees { get; set; }
 
         public EmployeeDatabase()
         {
             Employees = new ObservableCollection<Employee>();
-            LoadFromDatabase();
+            Load();
         }
 
         public int Add(Employee employee)
         {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            var res = wpfServiceSoapClient.Add(employee);
+            if (res > 0)
             {
-                connection.Open();
-
-                //TODO Добавить обязательную проверку на ввод полного имени
-                string sqlExpression = $@"INSERT INTO Employees (FirstName, LastName, MiddleName, Comment, OfficeCategory)
-                                     VALUES ('{employee.FirstName}', '{employee.LastName}', '{employee.MiddleName}', '{employee.Comment}', {(int)employee.OfficeCategory} )";
-                var command = new SqlCommand(sqlExpression, connection);
-                var res = command.ExecuteNonQuery();
-                if (res > 0)
-                {
-                    Employees.Add(employee);
-                }
-                return res;
+                Employees.Add(employee);
             }
+            return res;
         }
 
         //Пришлось добавить параметр employeeBefore, так как если нужно поменять имя, в параметр WHERE передавалось уже измененное ФИО, то есть мы изменяли то, чего нет
         public int Update(Employee employeeBefore, Employee employeeAfter)
         {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                connection.Open();
-
-                string sqlExpression = $@"UPDATE Employees 
-                    SET FirstName = '{employeeAfter.FirstName}', LastName = '{employeeAfter.LastName}', MiddleName = '{employeeAfter.MiddleName}', Comment = '{employeeAfter.Comment}', OfficeCategory = {(int)employeeAfter.OfficeCategory}
-                    WHERE FirstName = '{employeeBefore.FirstName}' AND LastName = '{employeeBefore.LastName}' AND MiddleName = '{employeeBefore.MiddleName}'";
-                var command = new SqlCommand(sqlExpression, connection);
-                return command.ExecuteNonQuery();
-            }
+            return wpfServiceSoapClient.Update(employeeBefore, employeeAfter);
         }
 
         public int Remove(Employee employee)
         {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            var res = wpfServiceSoapClient.Add(employee);
+            if (res > 0)
             {
-                connection.Open();
-
-                string sqlExpression = $@"DELETE FROM Employees WHERE FirstName = '{employee.FirstName}' AND LastName = '{employee.LastName}' AND MiddleName = '{employee.MiddleName}'";
-                var command = new SqlCommand(sqlExpression, connection);
-                var res = command.ExecuteNonQuery();
-                if (res > 0)
-                {
-                    Employees.Remove(employee);
-                }
-                return res;
+                Employees.Add(employee);
             }
+            return res;
         }
 
-        private void LoadFromDatabase()
+        private void Load()
         {
-            string sqlExpression = "SELECT * FROM Employees";
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            foreach (var employee in wpfServiceSoapClient.Load())
             {
-                connection.Open();
-                SqlCommand command = new SqlCommand(sqlExpression, connection);
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            var contact = new Employee()
-                            {
-                                FirstName = reader.GetValue(0).ToString(),
-                                LastName = reader["LastName"].ToString(),
-                                MiddleName = reader.GetString(2),
-                                Comment = reader["Comment"].ToString(),
-                                OfficeCategory = (Department)reader.GetInt32(4)
-                            };
-                            Employees.Add(contact);
-                        }
-                    }
-                }
+                Employees.Add(employee);
             }
         }
     }
